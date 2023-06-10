49eddc67b4bf806ba0e78495ef919d5cdca3d33b
MD2990 <31358358+MD2990@users.noreply.github.com>
2023-06-10 18:38:21 +0400

    cleaning up

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.querySelector(".menu");
-
-document.addEventListener("click", function (event) {
-  const target = event.target;
-  if (!menu.contains(target) && target !== menuToggle && menuToggle.checked) {
-    menuToggle.checked = false;
-  }
-});
